434125b6f93de14100bf4ca14f4f4fc78bbae27e
Sam W <sweldemariam@gmail.com>
2014-03-12 12:27:33 -0500

    added a registration

--- test/unit/user_spec.js
+++ test/unit/user_spec.js
@@ -1,5 +1,8 @@
+/* jshint expr:true */
+
 'use strict';
 
+var Mongo = require('mongodb');
 
 process.env.DBNAME = 'airbnb';
 var expect = require('chai').expect;
@@ -19,17 +22,45 @@ describe('User', function(){
 
   beforeEach(function(done){
     global.nss.db.dropDatabase(function(err, result){
-      done();
+      var u3 = new User({email:'adam@nomail.com', password:'1234', role:'host'});
+      u3.register(function(){
+        done();
+      });
     });
   });
 
   describe('new', function(){
     it('should create a new User object', function(done){
-      var u1 = new User({email:'adam@nomail.com', password:'1234', role:'host'});
-      expect(u1.email).to.equal('adam@nomail.com');
+      var u1 = new User({email:'adam2@nomail.com', password:'1234', role:'host'});
+      expect(u1.email).to.equal('adam2@nomail.com');
       expect(u1.password).to.equal('1234');
       expect(u1.role).to.equal('host');
       done();
+    });
+  });
+
+  describe('register', function(){
+    it('should register a new User', function(done){
+      var u1 = new User({email:'nsstestemail@yahoo.com', password:'1234', role:'host'});
+      u1.register(function(err, body){
+        expect(err).to.be.null;
+        expect(u1.password).to.have.length(60);
+        expect(u1._id).to.be.instanceof(Mongo.ObjectID);
+        body = JSON.parse(body);
+        expect(body.id).to.be.ok;
+        done();
+      });
+    });
+
+    it('should not register a new User because of duplicate email', function(done){
+      var u2 = new User({email:'nsstestemail@yahoo.com', password:'1234', role:'host'});
+      var u3 = new User({email:'nsstestemail@yahoo.com', password:'1234', role:'host'});
+      u2.register(function(err){
+        u3.register(function(err){
+          expect(u3._id).to.be.undefined;
+          done();
+        });
+      });
     });
   });
 });
